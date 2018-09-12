5557d33045186a7b7930add4253ad9699b297108
Jesse Hooker <jayhook87@gmail.com>
2018-09-12 16:25:27 -0400

    no bedtime

--- babysittin_money.rb
+++ babysittin_money.rb
@@ -4,13 +4,30 @@ class BabysittinMoney
 
   def gimme_my_money(start_time, bed_time, finish_time)
 
-    midnight = bed_time.beginning_of_day + 1.day
+    midnight = start_time.beginning_of_day + 1.day
 
-    start_to_bed = TimeDifference.between(start_time, bed_time).in_hours * 12
-    bed_to_midnight = TimeDifference.between(bed_time, midnight).in_hours * 8
-    makin_bank = TimeDifference.between(midnight, finish_time).in_hours * 16
+    if start_time.class == Time && bed_time.class == Time && finish_time.class == Time
+      start_to_bed = TimeDifference.between(start_time, bed_time).in_hours * 12
+      bed_to_midnight = TimeDifference.between(bed_time, midnight).in_hours * 8
+      making_bank = TimeDifference.between(midnight, finish_time).in_hours * 16
 
-   start_to_bed+bed_to_midnight+makin_bank
+      money_made(start_to_bed, bed_to_midnight, making_bank)
+
+    elsif bed_time == 0
+      start_to_bed = TimeDifference.between(start_time, finish_time).in_hours * 12
+      bed_to_midnight = 0
+      making_bank = 0
+
+      money_made(start_to_bed, bed_to_midnight, making_bank)
+
+    else
+      puts 'error bro'
+
+    end
+  end
+
+  def money_made(rate1, rate2, rate3)
+    rate1+rate2+rate3
   end
 
 end
--- spec/babysitting_money_spec.rb
+++ spec/babysitting_money_spec.rb
@@ -4,4 +4,9 @@ describe BabysittinMoney do
   it 'start at 5PM, bed at 9PM, parents arrive home drunk at 3AM' do
     expect(subject.gimme_my_money( Time.new(2018, 07, 04, 17),Time.new(2018, 07, 04, 21), Time.new(2018, 07, 05, 03))).to eq 120
   end
+
+  it 'kid never goes to bed' do
+    expect(subject.gimme_my_money(Time.new(2018, 07, 04, 17), 0, Time.new(2018, 07, 05, 03))).to eq 120
+  end
+
 end
