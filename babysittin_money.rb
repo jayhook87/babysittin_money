23848e2e5e37d04334c69a61ad47424feaae7933
Jesse Hooker <jayhook87@gmail.com>
2018-09-12 15:26:30 -0400

    working spec

--- babysittin_money.rb
+++ babysittin_money.rb
@@ -1,3 +1,16 @@
+require 'active_support'
+require 'time_difference'
 class BabysittinMoney
+
+  def gimme_my_money(start_time, bed_time, finish_time)
+
+    midnight = bed_time.beginning_of_day + 1.day
+
+    start_to_bed = TimeDifference.between(start_time, bed_time).in_hours * 12
+    bed_to_midnight = TimeDifference.between(bed_time, midnight).in_hours * 8
+    makin_bank = TimeDifference.between(midnight, finish_time).in_hours * 16
+
+   start_to_bed+bed_to_midnight+makin_bank
+  end
 
 end
--- spec/babysitting_money_spec.rb
+++ spec/babysitting_money_spec.rb
@@ -1,5 +1,7 @@
 require './babysittin_money.rb'
 
 describe BabysittinMoney do
-
+  it 'start at 5PM, bed at 9PM, parents arrive home drunk at 3AM' do
+    expect(subject.gimme_my_money( Time.new(2018, 07, 04, 17),Time.new(2018, 07, 04, 21), Time.new(2018, 07, 05, 03))).to eq 120
+  end
 end
