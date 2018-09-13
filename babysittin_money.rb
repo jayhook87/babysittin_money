bfebfd23baea799db1f30e98355834a2dfa3eae9
Jesse Hooker <jayhook87@gmail.com>
2018-09-13 11:49:31 -0400

    redudundancy is redundant

--- babysittin_money.rb
+++ babysittin_money.rb
@@ -11,19 +11,19 @@ class BabysittinMoney
       bed_to_midnight = TimeDifference.between(bed_time, midnight).in_hours * 8
       making_bank = TimeDifference.between(midnight, finish_time).in_hours * 16
 
-      money_made(start_to_bed, bed_to_midnight, making_bank)
 
     elsif bed_time == 0 && start_time.class == Time && finish_time.class == Time
       start_to_bed = TimeDifference.between(start_time, finish_time).in_hours * 12
       bed_to_midnight = 0
       making_bank = 0
 
-      money_made(start_to_bed, bed_to_midnight, making_bank)
 
     else
       return 'error bro'
 
     end
+    money_made(start_to_bed, bed_to_midnight, making_bank)
+
   end
 
   def money_made(rate1, rate2, rate3)
